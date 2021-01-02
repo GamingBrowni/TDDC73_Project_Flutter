133a09764ec8761ee6d2d558776ac040640fac6b
Ivanna <iva.63@hotmail.com>
2021-01-02 22:05:09 +0100

    Update

--- project_password/lib/main.dart
+++ project_password/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:project_password/passwordCheck.dart';
 
+// GLOBAL CONSTANTS - values of 'barValue' which passwordCheck.dart also uses
+const double weak = 40.0;
+const double good = 100.0;
+const double strong = 175.0;
+const double unbeatable = 200.0;
 
 void main() {
   runApp(MaterialApp(
@@ -13,19 +19,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   String password;
+  String passwordStrength = '';
+
   Color barColor = Color(0xFFD6D6D6); // grey[350]
+  Color barColorDefault = Color(0xFFD6D6D6); // grey[350]
   Color barColorRed = Color(0xFFF44336); // red
   Color barColorYellow = Color(0xFFFFFF00); // yellowAccent (yellow : EB3B)
   Color barColorGreen = Color(0xFF76FF03); // lightGreenAccent
   Color barColorDarkGreen = Color(0xFF1B5E20); // green[900]
-  String passwordStrength = '';
-  int barValue = 0;
+
+  double barValue = 0.0;
+
   bool buttonEnabled = false;
 
   @override
   Widget build(BuildContext context) {
+    // Enable/disable button
+    var _onPressed;
+    if (buttonEnabled) {
+      _onPressed = () {};
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
 
@@ -55,21 +70,21 @@ class _MyAppState extends State<MyApp> {
               child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.fromLTRB(10.0, 5.0, 0, 5.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 5.0, 0, 5.0),
                     alignment: Alignment.topLeft,
                     //color: Colors.blue,
                     child: Text(
                       'Enter password:',
-                          style: TextStyle(
+                      style: TextStyle(
                         fontSize: 16.0,
-                    ),
+                      ),
                     ),
                   ),
 
                   // -- PASSWORD INPUT ---
                   Container(
                     alignment: Alignment.topLeft,
-                    padding: EdgeInsets.fromLTRB(10.0, 0, 0, 5.0),
+                    padding: EdgeInsets.fromLTRB(15.0, 0, 0, 5.0),
                     //color: Colors.greenAccent,
                     child: ConstrainedBox(
                       constraints: BoxConstraints(
@@ -84,21 +99,68 @@ class _MyAppState extends State<MyApp> {
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
                         ),
-
                         onChanged: (passwordInput) {
-                          setState((){
-                            // Return barValue
+                          setState(() {
+                            // CALL passwordCheck & GET barValue
+                            // 'barValue' determines the strength of the password
+                            // Depending on 'barValue', change the appearance
+                            // & colors of text & strength-bar
+                            barValue = passwordCheck(passwordInput);
 
+                            // DEFAULT / NO INPUT YET
+                            if (barValue == 0.0) {
+                              barColor = barColorDefault;
+                              passwordStrength = '';
+                              buttonEnabled = false;
+                            }
+
+                            // WEAK
+                            if (barValue == weak) {
                               // Change barColor
-
+                              barColor = barColorRed;
                               // Change passwordStrength
+                              passwordStrength = 'Weak';
+                              // Change buttonEnabled true/false
+                              // if >OK → enable button, else false
+                              buttonEnabled = false;
+                            }
 
-                              // if >OK → enable button
+                            // GOOD
+                            if (barValue == good) {
+                              // Change barColor
+                              barColor = barColorYellow;
+                              // Change passwordStrength
+                              passwordStrength = 'Good';
+                              // Change buttonEnabled true/false
+                              // if >OK → enable button, else false
+                              buttonEnabled = true;
+                            }
 
+                            // STRONG
+                            if (barValue == strong) {
+                              // Change barColor
+                              barColor = barColorGreen;
+                              // Change passwordStrength
+                              passwordStrength = 'Strong';
+                              // Change buttonEnabled true/false
+                              // if >OK → enable button, else false
+                              buttonEnabled = true;
+                            }
+
+                            // UNBEATABLE
+                            if (barValue == unbeatable) {
+                              // Change barColor
+                              barColor = barColorDarkGreen;
+                              // Change passwordStrength
+                              passwordStrength = 'Unbeatable';
+                              // Change buttonEnabled true/false
+                              // if >OK → enable button, else false
+                              buttonEnabled = true;
+                            }
                           });
                         },
-                        ),
                       ),
+                    ),
                   ),
 
                   // --- STRENGTH METER ---------------------------------------
@@ -125,18 +187,62 @@ class _MyAppState extends State<MyApp> {
                             height: 15.0,
                             decoration: BoxDecoration(
                               color: Colors.grey[350],
-                              borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(6.0)),
                             ),
                           ),
                           Container(
-                            width: 40.0,
+                            width: barValue,
                             height: 15.0,
                             decoration: BoxDecoration(
                               color: barColor,
-                              borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(6.0)),
                             ),
                           ),
                         ],
+                      ),
+
+                      Container(
+                        padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+                        alignment: Alignment.topLeft,
+                        //color: Colors.blue[100],
+
+                        child: Text(
+                          passwordStrength,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            letterSpacing: 1.0,
+                            fontWeight: FontWeight.bold,
+                            color: barColor,
+                            shadows: <Shadow>[
+                              Shadow(
+                                offset: Offset(0.0, 0.0),
+                                blurRadius: 1.0,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                              Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 2.0,
+                                color: Color.fromARGB(100, 0, 0, 255),
+                              ),
+                            ],
+                          ),
+
+                        /*
+                        Text(
+                          passwordStrength,
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.bold,
+                              foreground: Paint()
+                                ..style = PaintingStyle.stroke
+                                ..strokeWidth = 2.0
+                                ..color = Colors.grey[600]),
+                        ),
+                        ),*/
+                        ),
                       ),
                     ],
                   ),
@@ -150,10 +256,10 @@ class _MyAppState extends State<MyApp> {
                     alignment: Alignment.center,
                     //color: Colors.blue[100],
                     child: RaisedButton(
-                      onPressed: () {},
+                      onPressed: _onPressed,
                       child: Text('Password passed!'),
-                      ),
                     ),
+                  ),
                 ],
               ),
             ),
@@ -163,5 +269,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-
--- project_password/lib/passwordCheck.dart
+++ project_password/lib/passwordCheck.dart
@@ -1,21 +1,30 @@
+import 'package:project_password/main.dart';
+
 double passwordCheck(String password) {
 
-  // If password field is empty, then the bar should be empty
-  if(password.isEmpty){
-    return 0.0;
-}
 
   // RED (WEAK) <=20% of the bar:
   // Letters a-zA-Z  OR  numbers only  AND  characters<8
-
-  // YELLOW (OK) <50% of the bar:
+  if (password.length < 3 && password.length > 1) {
+    return weak;
+  }
+  // YELLOW (OK) <=50% of the bar:
   // Letters a-zA-Z  AND  numbers  AND  characters>=8
-
+  else if (password.length < 5 && password.length > 2) {
+    return good;
+  }
   // GREEN (STRONG) <=85% of the bar:
   // Letters a-zA-Z  AND  numbers  AND  1 special character  AND  ch.>=8
-
-  // DARK GREEN (UNBEATABLE) >85% of the bar:
-  // Letters a-zA-Z  AND  numbers  AND  >1 special characters  AND  ch.>=10
-
-  return 1.0;
+  else if (password.length < 7 && password.length > 4) {
+    return strong;
+  }
+  // DARK GREEN (UNBEATABLE) 100% of the bar:
+  // Letters a-zA-Z  AND  numbers  AND  >=1 special characters  AND  ch.>=12
+  else if (password.length > 6) {
+    return unbeatable;
+  }
+  // If password field is empty, then the bar should be empty
+  else {
+    return 0.0;
+  }
 }
